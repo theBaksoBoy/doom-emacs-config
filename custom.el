c1266382bb8dbc73a2da22291355a54a69396e09
baksoBoy <128901211+theBaksoBoy@users.noreply.github.com>
2026-07-03 21:48:08 +0200

    remove unintended code
    I have no fucking clue how it got there as I didn't edit the file manually

--- custom.el
+++ custom.el
@@ -11,7 +11,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-level-1 ((t (:height 1.5 :weight bold))))
- '(org-level-2 ((t (:height 1.3 :weight bold))))
- '(org-level-3 ((t (:height 1.2))))
- '(org-level-4 ((t (:height 1.1)))))
+)
